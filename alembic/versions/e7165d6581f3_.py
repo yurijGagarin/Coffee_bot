9de79a66ecc0e7b513785cf4ab4fe15db5789c35
yurijGagarin <yurazhagan@gmail.com>
2022-06-18 12:09:54 +0300

    price fix

--- alembic/versions/e7165d6581f3_.py
+++ alembic/versions/e7165d6581f3_.py
@@ -139,7 +139,7 @@ def upgrade() -> None:
         {'name': 'Ванільний Еклер', 'price': 30, 'is_deserts': True},
         {'name': 'Шоколадний Еклер', 'price': 30, 'is_deserts': True},
         {'name': 'Фісташка-Малина Еклер', 'price': 40, 'is_deserts': True},
-        {'name': 'Чізкейк', 'price': 75, 'is_deserts': True},
+        {'name': 'Чізкейк', 'price': 65, 'is_deserts': True},
         {'name': 'Тірамісу', 'price': 60, 'is_deserts': True},
 
     ]
